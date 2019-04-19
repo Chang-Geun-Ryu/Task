e377d6c733ac0c144ca67b623628595cf5cebf52
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-04-19 21:05:57 +0900

    계산기 구현 시작2차

--- 2019.04.19/CalculatorExample - Starter/CalculatorExample/ViewController.swift
+++ 2019.04.19/CalculatorExample - Starter/CalculatorExample/ViewController.swift
@@ -11,33 +11,18 @@ import UIKit
 final class ViewController: UIViewController {
     @IBOutlet weak var result: UILabel!
     
-    @IBOutlet weak var num1: UIButton!
-    @IBOutlet weak var num2: UIButton!
-    @IBOutlet weak var num3: UIButton!
-    @IBOutlet weak var num4: UIButton!
-    @IBOutlet weak var num5: UIButton!
-    @IBOutlet weak var num6: UIButton!
-    @IBOutlet weak var num7: UIButton!
-    @IBOutlet weak var num8: UIButton!
-    @IBOutlet weak var num9: UIButton!
-    @IBOutlet weak var num0: UIButton!
-    
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var subButton: UIButton!
-    @IBOutlet weak var mulButton: UIButton!
-    @IBOutlet weak var divButton: UIButton!
-    
-    @IBOutlet weak var equlButton: UIButton!
-    @IBOutlet weak var clearButton: UIButton!
-    
     var store: Double = 0
-    var number: Double = 0
+    var number: Double? = 0
     var operatorClosure: ((Double, Double) -> Double)? = nil
     var addString: String = ""
+    var equalClicked = 0
+    let numformat = NumberFormatter()
     
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+        numformat.numberStyle = .decimal
+        numformat.minimumFractionDigits = 0
+        numformat.maximumFractionDigits = 3
         clear()
     }
     
@@ -45,49 +30,36 @@ final class ViewController: UIViewController {
         let alert = UIAlertController(title: "연산 가능한 한계 수를 초과했습니다.", message: "초기화 진행합니다.", preferredStyle: .alert)
         let ok = UIAlertAction(title: "확인", style: .default)
         alert.addAction(ok)
-        
         present(alert, animated: true)
     }
     
     func clear() {
         store = 0
         result.text = "0"
+        equalClicked = 0
         clearoperated()
     }
     func clearoperated() {
-        number = 0
+        number = nil
         operatorClosure = nil
         addString = ""
-        
     }
     
-    func equal() {
-        guard store != 0, number != 0, operatorClosure != nil else {return}
-        store = operatorClosure!(store, number)
-        
-        guard store <= Double.greatestFiniteMagnitude else {
-            print("!")
-            goAlert()
-            clear()
-            return
+    func equal(equalButtonClicked: Bool) {
+        if store == 0 {
+            store = number ?? 0
+            number = nil
+            addString = ""
+        } else if store != 0 && number == nil, equalButtonClicked{
+            number = store
         }
-        guard store >= Double.leastNormalMagnitude else {
-            print("@")
-            goAlert()
-            clear()
-            return}
         
-        print("4 store: \(store), number: \(number)")
-        print(Double.greatestFiniteMagnitude)
+        print("store: \(store), number: \(number ?? 0), equalClicked: \(equalClicked)")
+        guard store != 0, number != nil, equalClicked < 2, operatorClosure != nil else { print("FF", equalClicked); return }
         
-        let numformat = NumberFormatter()
-        numformat.numberStyle = .decimal
-        numformat.minimumFractionDigits = 0
-        numformat.maximumFractionDigits = 3
-        
-        result.text = numformat.string(from: store as NSNumber)
-        
-        clearoperated()
+        store = operatorClosure!(store, number!)                // 계산
+        result.text = numformat.string(from: store as NSNumber) // 자릿수에 맞춰 출력
+        clearoperated()                                         // 계산후 초기화
     }
     
     @IBAction func clearAll(_ sender: UIButton) {
@@ -101,37 +73,29 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func keyOperator(_ sender: UIButton) {
-        switch sender {
-        case addButton:
+        switch sender.titleLabel?.text {
+        case "+":
             operatorClosure = {$0 + $1}
-        case subButton:
+        case "-":
             operatorClosure = {$0 - $1}
-        case mulButton:
+        case "×":
             operatorClosure = {$0 * $1}
-        case divButton:
+        case "÷":
             operatorClosure = {$0 / $1}
         default:
             operatorClosure = nil
         }
-        print("1 store: \(store), number: \(number)")
         
-        if store == 0 {
-            store = number
-            number = 0
-            addString = ""
-        }
-        
-        print("2 store: \(store), number: \(number)")
-        equal()
+        equal(equalButtonClicked: false)
     }
     
     @IBAction func equaOperatorl(_ sender: UIButton) {
         guard operatorClosure != nil else {
-            print("a")
             clearoperated()
             return
         }
-        equal()
+    
+        equal(equalButtonClicked: true)
     }
     
     
